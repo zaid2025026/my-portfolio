fe4803d7e57874e876df5681765aca9070e1dc08
Zaid Abed <zaid2016404045@gmail.com>
2026-08-29 01:58:13 +0300

    تحديث ملف astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-// @ts-check
-
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zaid2025026.github.io',
+  base: '/my-portfolio',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
