4f9c1d0109c8dbeb55260980a37c82977ae1fc88
Zaid Abed <zaid2016404045@gmail.com>
2026-08-29 03:24:13 +0300

    Fix CSS output and responsive layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://zaid2025026.github.io',
-  base: '/my-portfolio',
-  build: {
-    assetsPrefix: '/my-portfolio'
-  },
+  base: '/my-portfolio/',
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: false // يدمج كل التنسيقات في ملف واحد يضمن عدم ضياعه
+    }
   },
 });
