f68242cdfaba328d1b8d911b552237c5fcee536b
Zaid Abed <zaid2016404045@gmail.com>
2026-08-30 12:48:20 +0300

    Fix layout and path base for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   site: 'https://zaid2025026.github.io',
   base: '/my-portfolio/',
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
