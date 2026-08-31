e1c65ede767bdf69907146af156fb24aa89d6442
Zaid Abed <zaid2016404045@gmail.com>
2026-08-31 10:13:19 +0300

    Remove base path for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://zaid2025026.github.io',
-  base: '/my-portfolio/',
+  // base: '/my-portfolio/',
 
   vite: {
     plugins: [tailwindcss()],
